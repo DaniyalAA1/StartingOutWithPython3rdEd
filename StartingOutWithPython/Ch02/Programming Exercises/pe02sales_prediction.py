298665e6ba09d397f7f59b043422b69f4219c08d
DaniyalAA1 <daniyalaalvi@gmail.com>
2023-05-02 11:23:46 -0700

    Completed second exercise

---
+++ StartingOutWithPython/Ch02/Programming Exercises/pe02sales_prediction.py
@@ -0,0 +1,12 @@
+# Prompt the user to enter the projected amount
+# of total sales
+# Create a program that finds the profit based
+# off 23 percent of the annual sales
+# Display the profits
+
+projected_sales = input('Enter your projected amount of total sales: ')
+profit = int(projected_sales) * 0.23
+print('Your profit based on your projected sales is', profit)
+
+# Michael dooing too much
+print(projected_sales + projected_sales)
