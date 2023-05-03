84bc760e838f809b1583eac9638107eaf4588118
DaniyalAA1 <daniyalaalvi@gmail.com>
2023-05-03 11:45:44 -0700

    Update pe03Land_calculation.py

--- StartingOutWithPython/Ch02/Programming Exercises/pe03Land_calculation.py
+++ StartingOutWithPython/Ch02/Programming Exercises/pe03Land_calculation.py
@@ -2,6 +2,7 @@
 # the total swuare feet in a tract of land
 # and calculates the number of acres in the tract
 
-total_square_feet = input('Enter the total square feet in your tract of land: ')
-acres = int(total_square_feet) // 43,560
-print('Based on your total square footage, you have', acres,'acres of land')
+total_square_feet = float(input('Enter the total square feet in your tract of land: '))
+acres = float(total_square_feet) / 43560
+print('Based on your total square footage, you have', 
+      format(acres, '.2f') ,'acres of land')
