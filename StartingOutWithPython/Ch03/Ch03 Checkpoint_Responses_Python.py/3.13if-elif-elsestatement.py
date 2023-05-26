a08c98f8ca03435fecfd92dddb455dbc383f131d
DaniyalAA1 <daniyalaalvi@gmail.com>
2023-05-26 10:08:39 -0700

    Checkpoint Question from CH03

---
+++ StartingOutWithPython/Ch03/Ch03 Checkpoint_Responses_Python.py/3.13if-elif-elsestatement.py
@@ -0,0 +1,23 @@
+# Convert the following code to an if-elif
+# -else statement
+# if-elif-else statement:
+#if number ==1
+#    print ('One)
+#else:
+#    if number ==2:
+#        print('Two)
+#    else: 
+#        if number ==3:
+#            print('Three)
+#        else:
+#            print('Unknown)
+
+
+if number == 1:
+    print('One')
+elif number == 2:
+    print('Two')
+elif number == 3:
+    print('Three')
+else:
+    print('Unknown')
