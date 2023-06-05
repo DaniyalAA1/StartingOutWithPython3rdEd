20a55c7c8a4dd2a651f5c1c3a7f79ac09b6bab96
DaniyalAA1 <daniyalaalvi@gmail.com>
2023-06-05 11:34:15 -0700

    Programming Exercise from CH03

---
+++ StartingOutWithPython/Ch03/Ch03 Progamming Examples/3.7loan_qualifier.py
@@ -0,0 +1,17 @@
+# This program determines whether a bank customner
+# qualifies for a loan.
+
+min_salary = 30000.0 # the minimum annual salary
+min_years = 2 # THe minimum years on the job
+
+# Get the customer's annual salary.
+salary=float(input('Enter your annual salary:'))
+
+# Get the number of years on the current job.
+years_on_job = int(input('Enter the number of' + 'years employed:'))
+
+# Determines whether the customer qualifies.
+if salary >= min_salary and years_on_job >= min_years:
+    print('You qualify for the loan.')
+else:
+    print('You do not qualify for this loan.')
